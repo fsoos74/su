31ef29402a42f645e96cabaffe67cc81c6e875ce
Filip Soos <fsoos@PC192-168-2-108.speedport.ip>
2016-10-16 19:51:12 +0200

    new default colormap

--- ColorUI/colortable.cpp
+++ ColorUI/colortable.cpp
@@ -99,12 +99,55 @@ void ColorTable::blend( QVector<QRgb>& cols, int first, int last){
 
 }
 
+// wave=380-780
+uint rgbFromWaveLength(double wave)
+{
+    double r = 0.0;
+    double g = 0.0;
+    double b = 0.0;
+
+    if (wave >= 380.0 && wave <= 440.0) {
+        r = -1.0 * (wave - 440.0) / (440.0 - 380.0);
+        b = 1.0;
+    } else if (wave >= 440.0 && wave <= 490.0) {
+        g = (wave - 440.0) / (490.0 - 440.0);
+        b = 1.0;
+    } else if (wave >= 490.0 && wave <= 510.0) {
+        g = 1.0;
+        b = -1.0 * (wave - 510.0) / (510.0 - 490.0);
+    } else if (wave >= 510.0 && wave <= 580.0) {
+        r = (wave - 510.0) / (580.0 - 510.0);
+        g = 1.0;
+    } else if (wave >= 580.0 && wave <= 645.0) {
+        r = 1.0;
+        g = -1.0 * (wave - 645.0) / (645.0 - 580.0);
+    } else if (wave >= 645.0 && wave <= 780.0) {
+        r = 1.0;
+    }
+
+    double s = 1.0;
+    if (wave > 700.0)
+        s = 0.3 + 0.7 * (780.0 - wave) / (780.0 - 700.0);
+    else if (wave <  420.0)
+        s = 0.3 + 0.7 * (wave - 380.0) / (420.0 - 380.0);
+
+    r = std::pow(r * s, 0.8);
+    g = std::pow(g * s, 0.8);
+    b = std::pow(b * s, 0.8);
+    return qRgb(int(r * 255), int(g * 255), int(b * 255));
+}
+
 QVector<QRgb> ColorTable::defaultColors(){
 
     const int DEFAULT_SIZE=256;
 
     QVector<QRgb> cols(DEFAULT_SIZE, qRgb(0,0,0));
 
+    for( int i=0; i<cols.size(); i++){
+        double lambda=380. + (780.-380.)/(cols.size()-1)*i;
+        cols[i]=rgbFromWaveLength(lambda);
+    }
+/*
     cols[0]=qRgb(255,0,64);
     cols[63]=qRgb(0,0,255);
     blend( cols, 0, 63);
@@ -116,6 +159,8 @@ QVector<QRgb> ColorTable::defaultColors(){
     blend(cols,127,191);
     cols[255]=qRgb(255,0,64);
     blend(cols,191,255);
+*/
+
 
     return cols;
 }
--- common.pri
+++ common.pri
@@ -1,6 +1,6 @@
 DEFINES += COMPANY=\\\"SeismicUtensils\\\"
 DEFINES += PROGRAM=\\\"AVO-Detect\\\"
-DEFINES += VERSION=\\\"1.2.1\\\"
+DEFINES += VERSION=\\\"1.2.2\\\"
 
 CONFIG += c++11
 
