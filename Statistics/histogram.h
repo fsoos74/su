26b2961f1cfc26d676c20100a9769fa24623ba30
Filip Soos <fsoos@seismicutensils.com>
2017-02-15 17:49:01 +0100

    - fill histogram with iterator pairs

--- Statistics/histogram.h
+++ Statistics/histogram.h
@@ -4,11 +4,17 @@
 
 #include<vector>
 #include<cstddef>   //size_t
+#include<algorithm>
+#include<limits>
 
 class Histogram
 {
 
 public:
+
+    typedef std::vector<size_t>::iterator iterator;
+    typedef std::vector<size_t>::const_iterator const_iterator;
+
 
     Histogram( const double& firstBin=0, const double& binIncr=1, size_t numberOfBins=1);
 
@@ -36,6 +42,10 @@ public:
         return m_totalCount;
     }
 
+    std::pair<double, double> range()const{
+        return std::make_pair( m_firstBin, m_firstBin + m_binWidth * m_binCount );
+    }
+
     // bounds of bin [min,max), throws error for out of bounds i
     std::pair<double,double> binInterval(const size_t i)const;
 
@@ -43,6 +53,38 @@ public:
     size_t binValue(const size_t)const;
 
     void addValue( const double& v);
+
+    template<typename ITERATOR> void addValues( ITERATOR begin, ITERATOR end){
+
+        for( ITERATOR it = begin; it!=end; ++it ){
+            addValue(*it);
+        }
+    }
+
+
+    template<typename ITERATOR, typename VALUE> void addValues( ITERATOR begin, ITERATOR end, VALUE NULL_VALUE){
+
+        for( ITERATOR it = begin; it!=end; ++it ){
+            if( *it!=NULL_VALUE) addValue(*it);
+        }
+    }
+
+
+    iterator begin(){
+        return m_bins.begin();
+    }
+
+    iterator end(){
+        return m_bins.end();
+    }
+
+    const_iterator cbegin()const{
+        return m_bins.cbegin();
+    }
+
+    const_iterator cend()const{
+        return m_bins.cend();
+    }
 
 private:
 
@@ -55,5 +97,26 @@ private:
 
     std::vector<size_t> m_bins;
 };
+
+template<typename ITERATOR, typename VALUE>
+Histogram createHistogram( ITERATOR begin, ITERATOR end, VALUE NULL_VALUE, size_t STEPS ){
+
+    auto min=std::numeric_limits<VALUE>::max();
+    auto max=std::numeric_limits<VALUE>::min();
+    for( auto it = begin; it!=end; ++it ){
+        if( *it==NULL_VALUE) continue;
+        if( *it<min ) min=*it;
+        if( *it>max ) max=*it;
+    }
+
+    if( min>=max ) return Histogram();
+
+    Histogram histogram( min, (max-min)/STEPS, STEPS);
+
+    histogram.addValues( begin, end, NULL_VALUE );
+
+    return histogram;
+}
+
 
 #endif // HISTOGRAM_H
